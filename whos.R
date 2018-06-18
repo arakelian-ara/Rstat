4c38310b481e3e714cb14a7738d15b36f653ac24
Ara Arakelian <arakelian-ara@users.noreply.github.com>
2018-06-18 17:57:41 +0200

    Add files via upload

--- whos.R
+++ whos.R
@@ -1,94 +1,99 @@
 #######################################################################
 # whos() function
 # look like matlab
-# v1.0 by Ara Arakelian (LSCE) February 2016
+# v1.1 by Ara Arakelian (LSCE) June 2018
 #######################################################################
 # whos()
 #######################################################################
-  whos <- function(...){
+whos <- function(Type='',niv_env=-1){
 # INPUT -------------------------------------------------------
-# can be empty (alphabetic without function)
-# combinaison of character s / r / f
-# s = order by size
-# r = reverse order
-# f = add function to the printed list
+# Type = string, can be
+#               empty (alphabetic without function)
+#               combinaison of character s / r / f
+#               s = order by size
+#               r = reverse order
+#               f = add function to the printed list
+# niv_env = integer
 # -------------------------------------------------------------
-a=sapply(ls(.GlobalEnv), function (object.name) object.size(get(object.name)));
-b=sapply(ls(.GlobalEnv), function (object.name) class(get(object.name)));
-# -------------------------------------------------------------
-arg=as.list(match.call());
-if      (length(arg)==1){ kind='';}
-else if (length(arg)==2){ kind=arg[[2]][1];}
-else {   stop('nothing OR one (and only one) string with combinaison of f r s characters only'); }
-rm(arg);
-# -------------------------------------------------------------
-F=0;S=0;R=0;
-if (nchar(kind)!=0){
-  if (grepl('f',kind,ignore.case=TRUE)==1){F=1};
-  if (grepl('s',kind,ignore.case=TRUE)==1){S=1};
-  if (grepl('r',kind,ignore.case=TRUE)==1){R=1};
-}
-# -------------------------------------------------------------
-     if (S==0){ order=1:length(a); } # alphabetic
-else if (S==1){ order=sort(as.integer(a),index.return=TRUE)$ix; }# by size
-if (R==1){ order=rev(order);} # reverse
-# -------------------------------------------------------------
-cat('Memory used in bytes (1octet=1byte=8bits)\n')
-if (F==1){
- cat('--- Variables --------------------------------\n',sep='');
-}
-for(i in order){
-  if(as.character(b[i])!="function"){
-    .whosNP(a[i]); .whosTY(a[i],b[i]); .whosLD(a[i]); cat('\n',sep='');
+  MyEnv=sys.frame(which=niv_env);
+  Oname=ls(env=MyEnv);
+  Osize=unname(sapply(ls(env=MyEnv), function (object.name) object.size(get(object.name,env=MyEnv))));
+  Oclas=unname(sapply(ls(env=MyEnv), function (object.name) class(get(object.name,env=MyEnv))));
+  # -------------------------------------------------------------
+  FF=0;SS=0;RR=0;
+  if (nchar(Type)!=0){
+    if (grepl('f',Type,ignore.case=TRUE)==1){FF=1};
+    if (grepl('s',Type,ignore.case=TRUE)==1){SS=1};
+    if (grepl('r',Type,ignore.case=TRUE)==1){RR=1};
   }
-}
-if (F==1){
-cat('--- Functions --------------------------------\n',sep='');
-for(i in order){
-  if(as.character(b[i])=="function"){
-    .whosNP(a[i]); .whosTY(a[i],b[i]); cat('\n',sep='');
+  # -------------------------------------------------------------
+  if (SS==0){order=1:length(Oname);} # alphabetic
+       else {order=sort(Osize,index.return=TRUE)$ix;}# by size
+  if (RR==1){order=rev(order);} # reverse
+  # -------------------------------------------------------------
+  cat('-------------------------------------------------------------------------\n',sep='');
+  cat('Memory used in bytes (1octet=1byte=8bits) | Environment ',sep='')
+  if (environmentName(MyEnv)=="R_GlobalEnv"){
+    cat('[R_GlobalEnv]',sep='')
   }
-}
-}
-rm(a,b);
+  else{
+    if (environmentName(MyEnv)!=""){
+      cat('[',environmentName(MyEnv),']')
+    }
+    else {
+      cat('[no name (local)]')
+    }
+  }
+  cat('\n');
+
+  cat('--- Variables -----------------------------------------------------------\n',sep='');
+  for(i in order){
+    if(as.character(Oclas[i])!="function"){
+      .whosNP(Oname[i],Osize[i]); .whosTY(Oclas[i]); .whosLD(Oname[i],Oclas[i],MyEnv); cat('\n',sep='');
+    }
+  }
+  if (FF==1){
+    cat('--- Functions -----------------------------------------------------------\n',sep='');
+    for(i in order){
+      if(as.character(Oclas[i])=="function"){
+        .whosNP(Oname[i],Osize[i]); .whosTY(Oclas[i]); cat('\n',sep='');
+      }
+    }
+  }
+  cat('-------------------------------------------------------------------------\n',sep='');
+  rm(Oname,Osize,Oclas);
+  invisible(gc());
 }
 #######################################################################
 # Whos : print name + weight
 #######################################################################
-  .whosNP <- function(x){
-cat(sprintf('%15s',names(x)),sep='');
-T=1024;
-if (x<=T){               cat(sprintf('%6.0f o',x)    ,sep='');}
-else if (x>T   & x<=T^2){cat(sprintf('%6.0f k',x/T)  ,sep='');}
-else if (x>T^2 & x<=T^3){cat(sprintf('%6.1f M',x/T^2),sep='');}
-else if (x>T^3){         cat(sprintf('%6.2f G',x/T^3),sep='');}
-}
-#######################################################################
-# Whos : print length or dimension 
-#######################################################################
-  .whosLD <- function(a){
-nvb=parse(text=names(a));
-if (is.character(eval(nvb)))  {cat(sprintf('%8d',nchar(eval(nvb)) ,sep='')); }
-else if (is.vector(eval(nvb))){cat(sprintf('%8d',length(eval(nvb)),sep='')); }
-else if (is.matrix(eval(nvb))){cat(sprintf('%8d x %d',dim(eval(nvb))[1],dim(eval(nvb))[2],sep='')); }
-else if (is.array (eval(nvb))){cat(sprintf('%8d',dim(eval(nvb))[1],sep=''));
-                               for (i in 2:length(dim(eval(nvb)))){
-                                cat(sprintf(' x %d',dim(eval(nvb))[i],sep=''));
-                               }
-}
-else { cat('   casetoadd');}
+.whosNP <- function(name,size){
+  cat(sprintf('%20s',name),sep='');
+  T=1024;
+  if (size<=T)                  {cat(sprintf('%6.0f o',size)    ,sep='');}
+  else if (size>T   & size<=T^2){cat(sprintf('%6.0f k',size/T)  ,sep='');}
+  else if (size>T^2 & size<=T^3){cat(sprintf('%6.1f M',size/T^2),sep='');}
+  else if (size>T^3)            {cat(sprintf('%6.2f G',size/T^3),sep='');}
 }
 #######################################################################
 # Whos : print type ie "kind"
 #######################################################################
-  .whosTY <- function(a,b){
-if (as.character(b)=='numeric'){ 
-  nvb=parse(text=names(a));
-  if      (is.vector(eval(nvb))){cat(sprintf('%11s','vector',sep=''));}
-  else if (is.array( eval(nvb))){cat(sprintf('%11s','array' ,sep=''));}
-} else {                         cat(sprintf('%11s',as.character(b)),sep='');
+.whosTY <- function(clas){
+  cat(sprintf('%11s',clas,sep=''));
 }
+#######################################################################
+# Whos : print length or dimension
+#######################################################################
+.whosLD <- function(name,clas,MyEnv){
+     if (clas=='character'){cat(sprintf('%8d',nchar(get(name,env=MyEnv)),sep=''));}
+else if (clas=='list')     {cat(sprintf('%8d',length(get(name,env=MyEnv)),sep='')); }
+else if (clas=='integer')  {cat(sprintf('%8d',length(get(name,env=MyEnv)),sep=''));}
+else if (clas=='matrix')   {cat(sprintf('%8d x %d',dim(get(name,env=MyEnv))[1],dim(get(name,env=MyEnv))[2],sep='')); }
+else if (clas=='array')    {cat(sprintf('%8d',dim(get(name,env=MyEnv))[1],sep=''));
+                              for (i in 2:length(dim(get(name,env=MyEnv)))){
+                                cat(sprintf(' x %d',dim(get(name,env=MyEnv))[i],sep=''));
+                              }
+                            }
+  else                      { cat('       case_to_add');}
 }
-
-
 
